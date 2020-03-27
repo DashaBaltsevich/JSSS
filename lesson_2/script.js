fdda73d6235280d1de64a3d84b9df806ac1481c3
Dasha Baltsevich <dasha.baltsevich@mail.ru>
2020-03-27 22:52:31 +0300

    сделала вторую домашку

--- lesson_2/script.js
+++ lesson_2/script.js
@@ -1,1 +1,21 @@
-console.log(1);
+let money = 100000,
+    income = 'верстка',
+    addExpenses = 'машина, квартира, курсы',
+    deposit = true,
+    mission = 100000000,
+    period = 1;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} долларов`);
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+
+let budgetDay = 5000;
+console.log(budgetDay);
