aab556401067b6356595880dc101ba62b8a07100
Dasha Baltsevich <dasha.baltsevich@mail.ru>
2020-03-28 00:29:15 +0300

    сделала усложненное задание

---
+++ lesson_2_hard/main.js
@@ -0,0 +1,17 @@
+let num = 266219;
+num = num + '';
+let arr = num.split('');
+let len = arr.length;
+let answ = 1;
+
+for (let i = 0; i < len; i++) {
+    answ *= arr[i];
+}
+
+console.log(answ);
+
+let result = (answ ** 3).toString().slice(0, 2);
+
+console.log(result);
+
+
