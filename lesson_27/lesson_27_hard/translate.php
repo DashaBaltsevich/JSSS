8a184d763813c4c745498506b9f88df21670b4ca
Dasha Baltsevich <dasha.baltsevich@mail.ru>
2020-05-06 20:40:57 +0300

    27 + доп

---
+++ lesson_27/lesson_27_hard/translate.php
@@ -0,0 +1,5 @@
+https://translate.yandex.net/api/v1.5/tr.json/getLangs
+ ? [key=<trnsl.1.1.20200506T151532Z.8dee65b5b3ec1b87.9d8850f1ae917a7a944f7574b52fd3490f556d08
+>]
+ & [ui=<en>]
+ & [callback=<trans>]
